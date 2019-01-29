9099df6ad3a7cb2b7eb8b52241eff6955140738f
Tobias Løfgren <tobloef@gmail.com>
2019-01-29 21:30:10 +0100

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -98,7 +98,7 @@ let chart = new Chart(ctx, {
     }
 });
 
-const socket = io();
+const socket = io("http://199.247.24.20:3000");
 socket.on("data", newData => {
     if (chart == null) {
         return;
